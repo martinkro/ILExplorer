47e297e3e90fa33e648c26b701f6f4c30512b1e4
sundayliu <martinkro@aliyun.com>
2017-03-22 20:20:31 +0800

    Initialize project

---
+++ ILExplorer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILExplorer
+{
+    class Program
+    {
+        private static string version = "1.0.1";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ILExplorer:" + version);
+        }
+    }
+}
