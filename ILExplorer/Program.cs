ca1fa1c676b0924d0f6430825c9b8a2b78e2b32e
sundayliu <martinkro@aliyun.com>
2017-03-24 17:33:56 +0800

    Test Reflector

--- ILExplorer/Program.cs
+++ ILExplorer/Program.cs
@@ -12,6 +12,27 @@ namespace ILExplorer
         static void Main(string[] args)
         {
             Console.WriteLine("ILExplorer:" + version);
+            TestReflector();
+        }
+
+        public static void TestReflector()
+        {
+            MonoReflector reflector = new MonoReflector();
+            var assemblyReflector = reflector.LoadAssembly("./testdata/Mono.Cecil.dll");
+            var types = assemblyReflector.GetTypes();
+            var attributes = assemblyReflector.GetAttributes<Attribute>();
+            foreach(var type in types)
+            {
+                Console.WriteLine("Type Name:" + type.FullName);
+            }
+
+            foreach(var attr in attributes)
+            {
+                foreach(var item in attr.Values){
+                    Console.WriteLine("Attribute:" + item.Key + ":" + item.Value);
+                }
+            }
+            
         }
     }
 }
