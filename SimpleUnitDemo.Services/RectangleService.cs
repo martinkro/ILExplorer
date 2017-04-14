ba7e1d3295a88296b86a8049d818042a11ef5d91
sundayliu <martinkro@aliyun.com>
2017-04-14 17:32:13 +0800

    unittest

---
+++ SimpleUnitDemo.Services/RectangleService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleUnitDemo.Services
+{
+    public class RectangleService
+    {
+        public double Area(double width, double height)
+        {
+            return width * height;
+        }
+
+        public double Primeter(double width, double height)
+        {
+            return 2 * (width + height);
+        }
+    }
+}
---
+++ SimpleUnitDemo.Tests/RectangleServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleUnitDemo.Services;
+using Xunit;
+
+namespace SimpleUnitDemo.Tests
+{
+    public class RectangleServiceTest
+    {
+        [Fact]
+        public void AreaTest()
+        {
+            RectangleService instance = new RectangleService();
+
+            var area = instance.Area(2, 3.5);
+
+            Assert.Equal(7.0, area);
+        }
+
+        [Fact]
+        public void OperationTest()
+        {
+            int a = 1;
+            int b = 2;
+            int c = a + b;
+            Assert.Equal(3, c);
+        }
+    }
+}
